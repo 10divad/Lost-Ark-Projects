5cc6ac4a791f4eba4a3ca03eaf14369950d79174
youroriginal <chienjanzen@gmail.com>
2022-04-29 01:38:04 -0400

    added 2 disc bot projects

---
+++ LADungeonTracker.py
@@ -0,0 +1,64 @@
+## HALT TILL WE HAVE API
+
+
+import discord
+import os
+import requests
+import json
+
+# For list we need - Abyss Dungeon ($), Guardian Raid (GR), Abyss Raid (AR), Legion Raid (LR), BardCDung (BCD), Cubes (CB), Boss Rush (BR), Platinum Field (PF), 
+
+$oreha3
+
+
+client = discord.Client()
+
+def get_quote
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello')
+
+    # line to start the environment (this is where u put discord server it runs on)
+    client.run(os.getenv('TOKEN'))
+
+
+    def on_message_add(user_input, user):
+
+        if user.message == 
+
+
+# !playerd
+    # output a list of everyones names and what they need
+
+# how would user remove a count?
+# would you like to auto remove 1 count in 30min
+
+
+
+# command !cube3
+
+# this would be input into a dictionary as: 
+    # user_dungeon_count = [david: {name_of_dungeon : count_user_needs},
+    # janzen: {name_of_dungeon : count}
+    # ]
+
+
+
+
+ 
+# 
+# 
+# take user input symbol as type and str as name
+
+# def function that takes 2 above paramaters
+
+    user_input = tostring function - user.message
+
+    try:
+        user_input.contains (symbols)
---
+++ QueueBotLA.py
@@ -0,0 +1,29 @@
+# import dependencies
+
+
+# queue list {name_of_dungeon : [user(s)]}
+
+# userinput - have 1 symbol (!) denote player in queue
+
+    # store userinput as variable
+
+    # loop through dict key and if user input matches a key:
+
+        # if player.queue_count for dungeon > 1:
+            # print player cant queue
+
+        # if count length of users < 4:
+            # append user name into list of users and name of dungeon
+
+        # elif count length of [users] >= 4:
+            # print f'sorry queue is full'
+
+        # elif user.input == 'leavequeue':
+            # name_of_list.remove(user_name)
+
+# !leavequeue
+
+    
+
+
+!argos
